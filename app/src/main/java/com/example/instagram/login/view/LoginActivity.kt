daad83318326f5eaacb41da45878fb6060a0adf5
Felipe Schoffen <fschoffen.dev@gmail.com>
2023-01-01 20:32:47 -0300

    login layout

---
+++ app/src/main/java/com/example/instagram/login/view/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.instagram.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instagram.R
+import com.example.instagram.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
