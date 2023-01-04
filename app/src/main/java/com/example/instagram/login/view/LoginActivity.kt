1b7c2b3fd25ec46cb404aa8d98dba93c21a32cef
Felipe Schoffen <fschoffen.dev@gmail.com>
2023-01-04 11:44:27 -0300

    custom enter login button, create register view files

--- app/src/main/java/com/example/instagram/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/instagram/login/view/LoginActivity.kt
@@ -2,6 +2,11 @@ package com.example.instagram.login.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.addTextChangedListener
 import com.example.instagram.R
 import com.example.instagram.databinding.ActivityLoginBinding
 
@@ -9,12 +14,38 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
+    private val watcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            binding.loginBtnEnter.isEnabled = s.toString().isNotEmpty()
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.loginEditEmailInput.error = "Esse e-mail é inválido."
+        val editTextEmail = binding.loginEditEmail
+        val editTextPassword = binding.loginEditPassword
+        val buttonEnter = binding.loginBtnEnter
+
+        editTextEmail.addTextChangedListener(watcher)
+
+        buttonEnter.setOnClickListener {
+            buttonEnter.showProgress(true)
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                buttonEnter.showProgress(false)
+            }, 2000)
+        }
 
     }
 }
