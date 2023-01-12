5df2e7288c25c49916ed6b0752ff59ce6760ec4d
Felipe Schoffen <fschoffen.dev@gmail.com>
2023-01-12 20:46:31 -0300

    profile view toolbar updated

--- app/src/main/java/com/example/instagram/main/view/MainActivity.kt
+++ app/src/main/java/com/example/instagram/main/view/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.instagram.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsetsController
+import androidx.core.content.ContextCompat
 import com.example.instagram.R
+import com.example.instagram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        window.insetsController?.setSystemBarsAppearance(
+//            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+//            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+//        )
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
+
+        setSupportActionBar(binding.mainToolbar)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_insta_camera)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
     }
 }
--- app/src/main/java/com/example/instagram/profile/view/FragmentProfile.kt
+++ app/src/main/java/com/example/instagram/profile/view/FragmentProfile.kt
@@ -1,15 +1,27 @@
 package com.example.instagram.profile.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.instagram.R
 import com.example.instagram.databinding.FragmentProfileBinding
 
 class FragmentProfile : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_profile, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +30,37 @@ class FragmentProfile : Fragment() {
     ): View {
         binding = FragmentProfileBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val rv = binding.profileRv
+        rv.layoutManager = GridLayoutManager(requireContext(), 3)
+        rv.adapter = PostAdapter()
+    }
+
+    private class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+            return PostViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.item_profile_grid, parent, false)
+            )
+        }
+
+        override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+            holder.bind(R.drawable.ic_insta_add)
+        }
+
+        override fun getItemCount(): Int {
+            return 30
+        }
+
+        private class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(image: Int) {
+                itemView.findViewById<ImageView>(R.id.item_profile_img_grid).setImageResource(image)
+            }
+        }
     }
 
 }
