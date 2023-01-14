98aa8586955611fef6980578fa3e09eef1ec5fb0
Felipe Schoffen <fschoffen.dev@gmail.com>
2023-01-13 22:36:11 -0300

    add search view

---
+++ app/src/main/java/com/example/instagram/search/view/FragmentSearch.kt
@@ -0,0 +1,66 @@
+package com.example.instagram.search.view
+
+import android.os.Bundle
+import android.view.*
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.instagram.R
+import com.example.instagram.databinding.FragmentSearchBinding
+
+class FragmentSearch : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_profile, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSearchBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val rv = binding.searchRv
+        rv.layoutManager = LinearLayoutManager(requireContext())
+        rv.adapter = PostAdapter()
+    }
+
+    private class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+            return PostViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.item_user_list, parent, false)
+            )
+        }
+
+        override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+            holder.bind(R.drawable.ic_insta_add)
+        }
+
+        override fun getItemCount(): Int {
+            return 30
+        }
+
+        private class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(image: Int) {
+                itemView.findViewById<ImageView>(R.id.search_img_user).setImageResource(image)
+            }
+        }
+    }
+
+}
